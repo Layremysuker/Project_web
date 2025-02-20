806517bd33ce381edb7b6f8250e184134b091397
unknown <ben054516@gmail.com>
2025-02-21 00:45:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      som :"#FF974E",
+      dum :"#282828",
+    }
+    },
+  },
+  plugins: [],
+}
