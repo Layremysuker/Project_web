84c1471e63e0c8906249c3d4798a918ad57cabb7
unknown <ben054516@gmail.com>
2025-02-21 18:00:32 +0700

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,18 @@
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        Kanit: ["Kanit", "serif"]
     },
     colors: {
       som :"#FF974E",
       dum :"#282828",
+    },
+    backgroundImage: {
+      'BG': "url('img/BG GOMODE.png')",
+      'BGG': "url('img/ky.png')",
+      'KY':"url('img/k6y.png')",
     }
+
     },
   },
   plugins: [],
