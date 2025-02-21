2c13b9b7d20c943361368111491f6c7769f2a21a
unknown <ben054516@gmail.com>
2025-02-21 18:38:05 +0700

    อัปเดตโค้ดใหม่

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@
     },
     backgroundImage: {
       'BG': "url('img/BG GOMODE.png')",
-      'BGG': "url('img/ky.png')",
+      'BGG': "url('img/KY.png')",
       'KY':"url('img/k6y.png')",
     }
 
